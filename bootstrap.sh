fcda9a130f8ffddde105c8e564e82538d66fa7e9
p.nazarychev <pavel.nazarychev@copper.co>
2023-11-18 23:05:46 +0000

    Bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,28 @@ set_ip () {
   TON_NODE_IP=$(curl -s https://ipinfo.io/ip)
 }
 
+build_all () {
+  docker-compose -f docker-compose.node.yml build
+}
+
 deploy_node () {
-    docker-compose -f docker-compose.node.yml up -d ton-node
+  docker-compose -f docker-compose.node.yml up -d ton-node
 }
 
 set_http_api_key () {
-    python -c 'import codecs; f=open("${NODE_STATE_VOLUME}/liteserver.pub", "rb+"); pub=f.read()[4:]; print(str(codecs.encode(pub,"base64")).replace("\n","")[2:46])'
+  NODE_API_KEY=$(docker run --r -v $NODE_STATE_VOLUME:/state python python -c 'import codecs; f=open('/state/liteserver.pub', "rb+"); pub=f.read()[4:]; print(str(codecs.encode(pub,"base64")).replace("\n","")[2:46])')
+}
+
+set_config_api_key () {
+  sed -i -e 's/NODEAPIKEY/'"$NODE_API_KEY"'/g' ./config/api-config/mainnet-config-onetomany.json
+}
+
+deploy_api () {
+  docker-compose -f docker-compose.node.yml up -d ton-api
 }
 
 set_ip
 source .env
 deploy_node
 set_http_api_key
+deploy_api
