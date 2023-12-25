a14fe8f28fdca45c049ed351367e99e3166e76b8
fmira21 <fmira21@example.com>
2023-12-25 11:16:03 +0000

    Multiple fixes

--- config/node-assets/init.sh
+++ config/node-assets/init.sh
@@ -15,7 +15,7 @@ if [ -f "./config.json" ]; then
     echo -e "\e[1;33m[=]\e[0m Found existing local config, skipping"
 else
     echo -e "\e[1;32m[+]\e[0m Using provided IP: $PUBLIC_IP:$CONSOLE_PORT"
-    validator-engine -C /var/ton-work/db/ton-global.config --db /var/ton-work/db --ip "$PUBLIC_IP:$CONSOLE_PORT"
+    /usr/bin/ton/validator-engine/validator-engine -C /var/ton-work/db/ton-global.config --db /var/ton-work/db --ip "$PUBLIC_IP:$CONSOLE_PORT"
 fi
 
 # Generating server certificate
@@ -23,7 +23,7 @@ if [ -f "./server" ]; then
     echo -e "\e[1;33m[=]\e[0m Found existing server certificate, skipping"
 else
     echo -e "\e[1;32m[+]\e[0m Generating and installing server certificate for remote control"
-    read -r SERVER_ID1 SERVER_ID2 <<< $(generate-random-id -m keys -n server)
+    read -r SERVER_ID1 SERVER_ID2 <<< $(/usr/bin/ton/utils/generate-random-id -m keys -n server)
     echo "Server IDs: $SERVER_ID1 $SERVER_ID2"
     cp server /var/ton-work/db/keyring/$SERVER_ID1
 fi
@@ -32,13 +32,13 @@ fi
 if [ -f "./client" ]; then
     echo -e "\e[1;33m[=]\e[0m Found existing client certificate, skipping"
 else
-    read -r CLIENT_ID1 CLIENT_ID2 <<< $(generate-random-id -m keys -n client)
+    read -r CLIENT_ID1 CLIENT_ID2 <<< $(/usr/bin/ton/utils/generate-random-id -m keys -n client)
     echo -e "\e[1;32m[+]\e[0m Generated client private certificate $CLIENT_ID1 $CLIENT_ID2"
     echo -e "\e[1;32m[+]\e[0m Generated client public certificate"
     # Adding client permissions
-    sed -e "s/CONSOLE-PORT/\"$(printf "%q" $CONSOLE_PORT)\"/g" -e "s~SERVER-ID~\"$(printf "%q" $SERVER_ID2)\"~g" -e "s~CLIENT-ID~\"$(printf "%q" $CLIENT_ID2)\"~g" control.template > control.new
-    sed -e "s~\"control\"\ \:\ \[~$(printf "%q" $(cat control.new))~g" config.json > config.json.new
-    mv config.json.new config.json
+    sed -e "s/CONSOLE-PORT/\"$(printf "%q" $CONSOLE_PORT)\"/g" -e "s~SERVER-ID~\"$(printf "%q" $SERVER_ID2)\"~g" -e "s~CLIENT-ID~\"$(printf "%q" $CLIENT_ID2)\"~g" /var/ton-work/db/control.template > /var/ton-work/db/control.new
+    sed -e "s~\"control\"\ \:\ \[~$(printf "%q" $(cat control.new))~g" /var/ton-work/db/config.json > /var/ton-work/db/config.json.new
+    mv /var/ton-work/db/config.json.new /var/ton-work/db/config.json
 fi
 
 # Liteserver
@@ -49,7 +49,7 @@ else
         echo -e "\e[1;33m[=]\e[0m Found existing liteserver certificate, skipping"
     else
         echo -e "\e[1;32m[+]\e[0m Generating and installing liteserver certificate for remote control"
-        read -r LITESERVER_ID1 LITESERVER_ID2 <<< $(generate-random-id -m keys -n liteserver)
+        read -r LITESERVER_ID1 LITESERVER_ID2 <<< $(/usr/bin/ton/utils/generate-random-id -m keys -n liteserver)
         echo "Liteserver IDs: $LITESERVER_ID1 $LITESERVER_ID2"
         cp liteserver /var/ton-work/db/keyring/$LITESERVER_ID1
         if [ -z "$LITE_PORT" ]; then
@@ -63,4 +63,4 @@ fi
 
 echo -e "\e[1;32m[+]\e[0m Running validator-engine"
 
-exec validator-engine -c /var/ton-work/db/config.json -C /var/ton-work/db/ton-global.config --db /var/ton-work/db
+exec /usr/bin/ton/validator-engine/validator-engine -c /var/ton-work/db/config.json -C /var/ton-work/db/ton-global.config --db /var/ton-work/db
